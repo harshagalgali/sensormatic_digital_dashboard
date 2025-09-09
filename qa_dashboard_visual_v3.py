ec45e4b1d172a97ed8ff82b746fd865e26457ae6
harsha <hharsha14@gmail.com>
2025-09-09 15:58:34 +0530

    Add files via upload

---
+++ qa_dashboard_visual_v3.py
@@ -0,0 +1,26 @@
+Streamlit.
+
+How to use Streamlit in 3 seconds:
+
+    1. Write an app
+    >>> import streamlit as st
+    >>> st.write(anything_you_want)
+
+    2. Run your app
+    $ streamlit run my_script.py
+
+    3. Use your app
+    A new tab will open on your browser. That's your Streamlit app!
+
+    4. Modify your code, save it, and watch changes live on your browser.
+
+Take a look at the other commands in this module to find out what else
+Streamlit can do:
+
+    >>> dir(streamlit)
+
+Or try running our "Hello World":
+
+    $ streamlit hello
+
+For more detailed info, see https://docs.streamlit.io.
